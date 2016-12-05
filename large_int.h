0306676ab7b9e1bc5fce586b097bb695aedd12e7
snk3y <d4tt423@gmail.com>
2016-12-06 02:30:17 +0900

    Update declaration order

--- large_int.h
+++ large_int.h
@@ -14,17 +14,18 @@ typedef struct {
 
 void init_large_int(LargeInt*); // LargeIntは最初に必ずこの関数で初期化する
 void hex_string_to_large_int(char*, LargeInt*);
-static unsigned int word_to_uint(char*, int, int);
-static unsigned int hex_char_to_uint(char);
-static void large_add(LargeInt*, LargeInt*, LargeInt*);
-static void large_sub(LargeInt*, LargeInt*, LargeInt*);
-static int is_less_than(LargeInt*, LargeInt*);
 void update_hex_string(LargeInt*);
 void update_binary_string(LargeInt*);
-static void swap(char*, char*);
-static void reverse_string(char*);
 void release_large_int(LargeInt*); // LargeIntは最後に必ずこの関数でメモリを開放する
 void print_number(LargeInt*);
 int get_digit(LargeInt*);
 void print_hex(LargeInt*);
 void print_binary(LargeInt*);
+
+static unsigned int word_to_uint(char*, int, int);
+static unsigned int hex_char_to_uint(char);
+static void large_add(LargeInt*, LargeInt*, LargeInt*);
+static void large_sub(LargeInt*, LargeInt*, LargeInt*);
+static int is_less_than(LargeInt*, LargeInt*);
+static void swap(char*, char*);
+static void reverse_string(char*);
