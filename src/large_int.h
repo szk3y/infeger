91f3ff6828bb118e449c0c738112e2f7e6565174
snk3y <d4tt423@gmail.com>
2016-12-07 16:26:11 +0900

    Update directory name

---
+++ src/large_int.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "list.h"
+
+// char*型の要素に直接代入をしてはいけない
+// 必ずupdate関数を使うこと
+typedef struct {
+    int is_negative;
+    List unsigned_value;
+    char* decimal_string;
+    char* binary_string;
+    char* hex_string;
+} LargeInt;
+
+void init_large_int(LargeInt*); // LargeIntは最初に必ずこの関数で初期化する
+void copy_large_int(LargeInt*, LargeInt*); // copy_large_int(a,b) => b = a
+void hex_string_to_large_int(char*, LargeInt*);
+void update_hex_string(LargeInt*);
+void update_binary_string(LargeInt*);
+void release_large_int(LargeInt*); // LargeIntは最後に必ずこの関数でメモリを開放する
+void print_number(LargeInt*);
+int get_digit(LargeInt*);
+void print_hex(LargeInt*);
+void print_binary(LargeInt*);
+
+static unsigned int word_to_uint(char*, int, int);
+static unsigned int hex_char_to_uint(char);
+void large_add(LargeInt*, LargeInt*, LargeInt*);
+static void large_sub(LargeInt*, LargeInt*, LargeInt*);
+static int is_less_than(LargeInt*, LargeInt*);
+static void swap(char*, char*);
+static void reverse_string(char*);
---
+++ src/list.h
@@ -0,0 +1,24 @@
+#pragma once
+
+typedef struct node Node;
+typedef struct node {
+    unsigned int key;
+    Node* next_node;
+    Node* prev_node;
+} Node;
+
+typedef struct {
+    Node* head;
+    Node* last;
+} List;
+
+void init_node(Node*); // Nodeは最初に必ずこの関数を使って初期化する
+void init_list(List*); // Listは最初に必ずこの関数を使って初期化する
+void copy_list(List*, List*); // copy_list(a,b) => b = a
+int is_empty(List*);
+int get_length(List*); // ノードの数を返す
+void push_back(List*, unsigned int); // Listの後ろに新しいノードをつける
+void push_front(List*, unsigned int); // Listの前に新しいノードをつける
+Node* securely_get_prev_node(Node*); // NULLを渡されると停止せずNULLを返す
+unsigned int securely_get_value(Node*); // NULLを渡されると停止せず0を返す
+void release_list(List*); // Listは最後に必ずこの関数を使ってメモリを開放する
